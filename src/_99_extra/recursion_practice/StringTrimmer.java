b1c0d8f61d34db3110dc4cc0b0da4defd002f960
HelloThereGeneralKenobi <37271141+HelloThereGeneralKenobi@users.noreply.github.com>
2020-05-10 17:17:09 -0700

    Finished Module 2 :)

--- src/_99_extra/recursion_practice/StringTrimmer.java
+++ src/_99_extra/recursion_practice/StringTrimmer.java
@@ -2,18 +2,25 @@ package _99_extra.recursion_practice;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 public class StringTrimmer {
 	
 	static public String trimString(String str, int removesLeft) {
 		//1. If there are no removes left
 			//2.  return the string
+		if(removesLeft == 0) {
+			return str;
+		}
 		
 		//3. Else you need to substring the string by 1, reduce removesLeft by 1, and then return both. 
+		else {
+			return trimString(str.substring(0,str.length() - 1),removesLeft - 1);
+		}
 		
-		return null;
 	}
-	
-	public static void main(String[] args) {
+	@Test
+	public void test() {
 		assertEquals("Hello W", trimString("Hello World!", 5));
 		assertEquals("League", trimString("LeagueOfAmazing!", 10));
 		assertEquals("James", trimString("JamesTheIntern", 9));
